3bbf7ee6d5abcd4157e9cf7059a5ae9f9fb5cc70
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-12-25 00:17:44 +0100

    Refactored auth routes to use imported functions from authController

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,8 @@
 const express = require("express");
+const { signup, login } = require("../controllers/authController");
 const router = express.Router();
 
-// Example routes
-router.post("/signup", (req, res) => {
-  res.send("Signup route");
-});
-
-router.post("/login", (req, res) => {
-  res.send("Login route");
-});
+router.post("/signup", signup);
+router.post("/login", login);
 
 module.exports = router;
